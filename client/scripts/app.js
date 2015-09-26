998770f7780a1e5cc5626d6d93224ccb167c470d
Ryan M <rm.main@gmail.com>
2015-09-26 17:31:01 -0500

    first commit

---
+++ client/scripts/app.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function() {
+
+    $.ajax('/getData', {
+        success: function(response){
+            console.log(response);
+            $(function () {
+                // Grab the template script
+                var theTemplateScript = $("#entry-template").html();
+
+                // Compile the template
+                var theTemplate = Handlebars.compile(theTemplateScript);
+
+                // Pass our data to the template
+                var theCompiledHtml = theTemplate(response);
+
+                // Add the compiled html to the page
+                $('body').html(theCompiledHtml);
+            });//end function()
+
+            //This was for appending to the DOM
+            // for(var it = 0; it < response.length; it++){
+            //   $(".content-placeholder").append("<p>" + response[it].color + " " + response[it].value + "</p>");
+            // }
+        },//end success
+        error: function(request, errorType, errorMessage){
+            alert(errorType + " " + errorMessage);
+        }
+    });//end ajax
+
+
+
+
+})//end ready
